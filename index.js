26b9a6e8d23eb4057f570dc3d2bb1f00e8d960a4
Michael Robinson <mike@pagesofinterest.net>
2013-08-24 14:38:47 +1200

    Updated index

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 module.exports = {
-  initialise: require('./lib/initialise')
+  initialise: require('./lib/initialise'),
+  collector: require('./lib/collector'),
+  prepareRenderer: require('./lib/visor').prepareRenderer
 }
