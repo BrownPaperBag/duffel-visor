83151cc099678446957ad71520f01d943ba924b1
Michael Robinson <mike@pagesofinterest.net>
2013-08-24 23:23:28 +1200

    Added GemFile and config.rb for Compass

---
+++ config.rb
@@ -0,0 +1,8 @@
+preferred_syntax = :scss
+http_path = '/'
+css_dir = 'public/stylesheets'
+sass_dir = 'private/scss'
+images_dir = 'assets/images'
+relative_assets = false
+line_comments = true
+output_style = :expanded
