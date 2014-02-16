de9b6169c0f96706ac645bba32b474fcdd46fb5d
Michael Robinson <mike@pagesofinterest.net>
2014-02-16 23:06:43 +1300

    Removed css

--- config.rb
+++
@@ -1,8 +0,0 @@
-preferred_syntax = :scss
-output_style = :expanded
-http_path = '/'
-css_dir = 'private/css'
-sass_dir = 'private/scss'
-images_dir = 'assets/images'
-relative_assets = false
-line_comments = true
