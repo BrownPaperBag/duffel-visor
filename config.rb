4fcadce897c1562aaef0196d2da2f092ac96e1bc
Michael Robinson <mike@pagesofinterest.net>
2014-02-16 23:03:57 +1300

    Updated css path

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 preferred_syntax = :scss
 output_style = :expanded
 http_path = '/'
-css_dir = 'private/stylesheets'
+css_dir = 'private/css'
 sass_dir = 'private/scss'
 images_dir = 'assets/images'
 relative_assets = false
