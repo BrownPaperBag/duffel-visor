243aca79687daf1ea8a1b56219ffb9d8e9ea7d88
Michael Robinson <mike@pagesofinterest.net>
2014-02-16 21:37:36 +1300

    Updated config.rb

--- config.rb
+++ config.rb
@@ -1,8 +1,8 @@
 preferred_syntax = :scss
+output_style = :expanded
 http_path = '/'
-css_dir = 'public/stylesheets'
+css_dir = 'private/stylesheets'
 sass_dir = 'private/scss'
 images_dir = 'assets/images'
 relative_assets = false
 line_comments = true
-output_style = :expanded
