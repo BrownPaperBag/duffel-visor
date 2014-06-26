0882f682c9caa5fc39d14313a87f278a9e24a168
Michael Robinson <mike@pagesofinterest.net>
2014-06-27 09:45:50 +1200

    Added sass config

---
+++ config.rb
@@ -0,0 +1,9 @@
+preferred_syntax = :scss
+output_style = :expanded
+http_path = '/'
+css_dir = 'private/css'
+sass_dir = 'private/scss'
+images_dir = 'public/images'
+relative_assets = false
+line_comments = true
+
