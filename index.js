bc91e764a16931ad081149126cf450d4a43d62d7
Michael Robinson <mike@pagesofinterest.net>
2014-06-27 09:44:31 +1200

    Removed reference to visor renderer

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = {
   initialise: require('./lib/initialise'),
-  collector: require('./lib/collector'),
-  prepareRenderer: require('./lib/visor').prepareRenderer
-}
+  collector: require('./lib/collector')
+};
